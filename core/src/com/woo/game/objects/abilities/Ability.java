1d020c320addb8065c130dc3194e26e7d0b6f3af
emptyloominaty <cipuksiblol@gmail.com>
2023-01-13 15:06:00 +0100

    5

--- core/src/com/woo/game/objects/abilities/Ability.java
+++ core/src/com/woo/game/objects/abilities/Ability.java
@@ -3,14 +3,14 @@ package com.woo.game.objects.abilities;
 import com.woo.game.objects.gameobjects.Creature;
 
 public class Ability {
-    String name;
+/*    String name;
     double gcd;
     double cd;
     double maxCd;
     boolean channeling;
     boolean casting;
     boolean canMove;
-    boolean type; //fire,frost,physical,nature
+    String type; //fire,frost,physical,nature
     int charges = 1;
     int maxCharges = 1;
     //TODO: effects arraylist? custom object?
@@ -71,7 +71,7 @@ public class Ability {
         return true;
     }
 
-    public void runBuff(Creature target,/*Buff, */int id) {
+    public void runBuff(Creature target, TODO Buff,int id) {
 
     }
 
@@ -96,12 +96,85 @@ public class Ability {
     }
 
     public boolean checkStart(Creature caster, double cost, int secCost) {
-
-        
+        if (talent != talentSelected) {
+            return false;
+        }
+        if (caster.isStunned || caster.isDead || (caster.isInterrupted && type.equals("Physical"))) {
+            return false;
+        }
+        if (canUse && checkGcd(caster) && !caster.isInterrupted && checkCost(caster,cost,secCost) && checkCasting(caster && checkCd(caster) && abilityCd>=abilityMaxCd && checkRooted(caster))) {
+            return true;
+        }
         return false;
     }
 
+    public boolean checkRooted(Creature caster) {
+        if (!canCastWhileRooted && caster.isRooted) {
+            return false;
+        }
+        return true;
+    }
 
+    public boolean checkGcd(Creature caster) {
+        if (!noGcd) {
+            if (caster.gcd <= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
 
+    public boolean checkCasting(Creature caster) {
+        if (!noGcd) {
+            if (caster.isCasting) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
 
+    public void setCd() {
+        if (maxCharges>1) {
+            if (charges==maxCharges) {
+                this.cd = 0;
+            }
+            charges--;
+        } else {
+            cd = 0;
+        }
+    }
+
+    public void setGcd(Creature caster,double gcd) { //gcd = 0
+        //TODO:
+    }
+
+    public boolean isEnemy(Creature caster, Creature target) {
+        //TODO
+        return false;
+    }
+
+    public boolean checkDistance(Creature caster, Creature target, int rangeP,boolean dontShow) { //range = 0, dontShow = false
+        if (rangeP===0) {
+            rangeP = this.range;
+        }
+        if (getDistance(caster,target)>rangeP) {
+            if (caster==player && !dontShow) {
+                //_message.update("Out of range", 2, colors.error)
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean canSpellQueue(Creature caster) {
+        return (caster==player && caster.gcd<spellQueueWindow && (caster.gcd>0 || caster.isCasting));
+    }
+*/
 }
